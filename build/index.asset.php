3b6c9cc8af50792eabe84fcc0acd95d439e523a8
dekode-this <daniel@dekode.no>
2022-12-08 09:25:29 +0100

    Frontend - added negative margin to align to edges. Added h4 and p styles Backend - Created same styles for backend because the markup is different.

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '45df84c10b1243b48ae49102bb1664c7');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '2ecdd9937d3e5bfc63d4b34e626fbddd');
