2aa78789e81112c05261a8e76244f3565f7a67d1
dekode-this <daniel@dekode.no>
2022-12-08 12:15:27 +0100

    Added styles for the plus button.

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '8eb6ebeabea6af904ff8b8a1f95ef2e6');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'e83137a324828c782bf8876a1810fe26');
